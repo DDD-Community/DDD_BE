5f3b1834ff9cdeddce466db8aa4457e908283d86
JSL107 <juneseok0107@naver.com>
2026-03-18 14:00:21 +0900

    fix: 서버 포트번호 로그로 출력

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+  const port = Number(process.env.PORT ?? 3000);
+
+  await app.listen(port);
+
+  const appUrl = await app.getUrl();
+  Logger.log(`Server is running on ${appUrl}`, 'Bootstrap');
+};
+
+void bootstrap();
