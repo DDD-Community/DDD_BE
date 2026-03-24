4723fec731ed388fbd0adc0507a924d6fef3f3d7
JSL107 <juneseok0107@naver.com>
2026-03-24 17:19:24 +0900

    style: format import 정렬

--- src/google/google.module.ts
+++ src/google/google.module.ts
@@ -4,8 +4,8 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
 import { GoogleAuthService } from './application/google-auth.service';
-import { GoogleApiClient } from './infrastructure/google-api.client';
 import { GoogleStrategy } from './infrastructure/google.strategy';
+import { GoogleApiClient } from './infrastructure/google-api.client';
 import { GoogleAuthController } from './interface/google-auth.controller';
 
 @Module({
